c2a82d2736265b90e4c5123bd8ba7e615c9a6d78
shekhuverma <shekharvrm47@gmail.com>
2017-06-13 11:24:42 +0530

    Added decission tree

---
+++ Part 2 - Regression/Section 8 - Decision Tree Regression/my_decission_tree.py
@@ -0,0 +1,34 @@
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+x = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+# Fitting the Regression Model to the dataset
+from sklearn.tree import DecisionTreeRegressor
+regressor=DecisionTreeRegressor(random_state=0)
+regressor.fit(x,y)
+# Predicting a new result
+y_pred = regressor.predict(6.5)
+
+# Visualising the Regression results
+plt.scatter(x, y, color = 'red')
+plt.plot(x, regressor.predict(x), color = 'blue')
+plt.title('Truth or Bluff (decission tree Model)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualising the Regression results (for higher resolution and smoother curve)
+x_grid = np.arange(min(x), max(x), 0.01)
+x_grid = x_grid.reshape((len(x_grid), 1))
+plt.scatter(x, y, color = 'red')
+plt.plot(x_grid, regressor.predict(x_grid), color = 'blue')
+plt.title('Truth or Bluff (Regression Model)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()# Regression Template
